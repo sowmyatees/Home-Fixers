dfcbb3968e0f8dc616c0dbd358edbba136a8789f
sowmyatees <sowmyatees@gmail.com>
2021-05-07 02:45:34 +0530

    created login and register

---
+++ app/src/main/java/uk/ac/tees/aad/W9456492/CreateAccount.java
@@ -0,0 +1,15 @@
+package uk.ac.tees.aad.W9456492;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CreateAccount extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_account);
+    }
+
+}
---
+++ app/src/main/java/uk/ac/tees/aad/W9456492/LoginActivity.java
@@ -0,0 +1,108 @@
+package uk.ac.tees.aad.W9456492;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.regex.Pattern;
+
+public class LoginActivity extends AppCompatActivity {
+
+    FirebaseAuth firebaseAuth;
+    FirebaseUser firebaseUser;
+    EditText email;
+    EditText password;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+        firebaseUser  = firebaseAuth.getCurrentUser();
+
+        email = findViewById(R.id.email);
+        password = findViewById(R.id.password);
+        Button create_account = findViewById(R.id.createAccount);
+
+
+        Button login = findViewById(R.id.login);
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                login();
+            }
+        });
+
+        create_account.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(),MainActivity.class));
+            }
+        });
+    }
+
+    private void login() {
+
+       validate();
+
+       FirebaseAuth.getInstance().signInWithEmailAndPassword(email.getText().toString(),password.getText().toString()).addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+           @Override
+           public void onSuccess(AuthResult authResult) {
+               firebaseUser = authResult.getUser();
+           }
+       }).addOnFailureListener(new OnFailureListener() {
+           @Override
+           public void onFailure(@NonNull Exception e) {
+               firebaseUser = null;
+           }
+       });
+    }
+
+    private void validate() {
+        if(email.getText().toString().isEmpty()){
+            email.setError("Cannot be empty");
+            email.findFocus();
+        }
+        if(!Patterns.EMAIL_ADDRESS.matcher(email.getText().toString()).matches()){
+            email.setError("Enter valid Email");
+            email.findFocus();
+        }
+        if(password.getText().toString().isEmpty())
+        {
+            password.setError("Cannot be empty");
+            password.findFocus();
+        }
+        if(password.getText().toString().length()<6)
+        {
+            password.setError("More than 6 chars");
+            password.findFocus();
+        }
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        if(FirebaseAuth.getInstance().getCurrentUser() == null){
+            startActivity(new Intent(this,CreateAccount.class));
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        finish();
+    }
+}
